a6030f8533c69b47189d7bd564754eb57668e320
Alexey Kravtsov <work_fando@mail.ru>
2024-08-04 12:08:50 +0300

    character can walk now

---
+++ Assets/Scripts/Animation/Character/Component/AnimationComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Animation.Character.Component
+{
+    public class AnimationComponent : MonoBehaviour
+    {
+        private const string StopTrigger = "stop";
+        private const string GoHorizontalTrigger = "walk_horizontal";
+        private const string GoUpTrigger = "walk_up";
+        private const string GoDownTrigger = "walk_down";
+
+        [SerializeField] private Animator _animator;
+        [SerializeField] private Transform _targetTransform;
+
+        public void Stop()
+        {
+            _animator.SetTrigger(StopTrigger);
+        }
+
+        public void GoHorizontal(float xDirection)
+        {
+            _animator.SetTrigger(GoHorizontalTrigger);
+            
+            var initialScale = _targetTransform.localScale;
+            
+            _targetTransform.localScale = new Vector3(Mathf.Sign(xDirection) * Mathf.Abs(initialScale.x),
+                initialScale.y, initialScale.z);
+        }
+
+        public void GoUp()
+        {
+            _animator.SetTrigger(GoUpTrigger);
+        }
+
+        public void GoDown()
+        {
+            _animator.SetTrigger(GoDownTrigger);
+        }
+    }
+}
--- Assets/Scripts/Movement/MovementComponent.cs
+++ Assets/Scripts/Movement/MovementComponent.cs
@@ -1,3 +1,4 @@
+using Animation.Character.Component;
 using UnityEngine;
 
 namespace Movement
@@ -6,9 +7,39 @@ namespace Movement
     {
         [SerializeField] private float _movementSpeed;
         
+        private AnimationComponent _animationComponent;
+
+        private void Awake()
+        {
+            _animationComponent = GetComponent<AnimationComponent>();
+        }
+
         public void MoveDirection(Vector3 direction)
         {
             transform.position += direction * (_movementSpeed * Time.deltaTime);
+            
+            if (direction.magnitude == 0)
+            {
+                _animationComponent.Stop();
+            }
+            else
+            {
+                if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                {
+                    _animationComponent.GoHorizontal(direction.x);
+                }
+                else
+                {
+                    if (direction.y >= 0)
+                    {
+                        _animationComponent.GoUp();
+                    }
+                    else
+                    {
+                        _animationComponent.GoDown();
+                    }
+                }
+            }
         }
     }
 }
